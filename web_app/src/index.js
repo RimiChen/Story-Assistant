8ade1eb0497c71ff64b317b8982aeb745dfc9746
Yi-Chun Chen <ychen74@ncsu.edu>
2017-02-24 11:51:23 -0500

    update sample interface & document
    interface implementation (part of)
    NLTK note

---
+++ web_app/src/index.js
@@ -0,0 +1,68 @@
+// Revealing module pattern
+var storyMenuFunctions = (function () {
+    
+  // Keep this variable private inside this closure scope
+  var storyNumber = 3;
+  
+  var importStory = function() {
+    console.log("import Story");
+    var bookIconPath = './img/Book.png';
+    for(var storyNumberIter = 0; storyNumberIter < storyNumber; storyNumberIter++){
+      drawCanvas(storyNumberIter, storyNumberIter * 200, bookIconPath);
+    }
+    var writeIconPath = './img/icon_Create.png';
+    drawCanvas(storyNumber, storyNumber * 200, writeIconPath);
+    
+    clickCanvas();
+  };
+
+  var createStory = function() {
+/*
+    var failingGrades = myGrades.filter(function(item) {
+        return item < 70;
+      });
+*/
+
+    return 'Create a new story ' + '.';
+  };
+
+  // Explicitly reveal public pointers to the private functions 
+  // that we want to reveal publicly
+  var drawCanvas = function( newIndex, shift, imgPath){
+    var newBook = {
+        canvas : document.createElement("canvas"),
+        draw : function() {
+            this.canvas.id = "canvas"+newIndex;
+            this.canvas.width = 128;
+            this.canvas.height = 128;
+            this.context = this.canvas.getContext("2d");
+            document.body.insertBefore(this.canvas, document.body.childNodes[newIndex]);
+            
+        }
+    }
+    
+    newBook.draw();
+    
+    var ctx = document.getElementById("canvas"+newIndex).getContext('2d');
+    var img = new Image();
+    img.onload = function() {
+      ctx.drawImage(img, 0, 0);
+    };
+    img.src = imgPath;
+  };
+  
+  var clickCanvas = function(){
+    $(document).ready(function(){
+      $("p").click(function(){
+          $(this).hide();
+      });
+    });
+  };
+
+  return {
+    importStory: importStory,
+    createStory: createStory,
+    drawCanvas: drawCanvas,
+    clickCanvas: clickCanvas
+  }
+})();
