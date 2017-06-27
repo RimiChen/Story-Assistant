f92d7f1565c6467e8ad1ebfc09d57f2f7b06d3d3
RimiChen <ychen74@ncsu.edu>
2017-06-27 11:23:43 -0400

    update rensa
    update rensa

--- Test_Sample/rensapy/rensapy/demos/test_transform_text_rensa.py
+++ Test_Sample/rensapy/rensapy/demos/test_transform_text_rensa.py
@@ -29,24 +29,54 @@ def main(inputString):
     pp = pprint.PrettyPrinter(indent=4)
     #pp.pprint(learned)
     
-    key = 'John'
+    #key = 'John'
     #'Elinor'
     #'Henry'
     current_assertion_list = {};
-    if key in learned_separate:
-        print("Actor: " +key)
-        Rensa = make_brain(sum(learned_separate[key],[]))
-        current_assertion_list = get_actor_assertions(key, Rensa)
-        new_list = delete_assertion(current_assertion_list, Rensa)
+    json_data=open(sys.argv[1])
+    jdata = json.load(json_data)
+    
+    for key in jdata.items():
+        actor_key = str(key[0])
+        print(actor_key+"!")
+    
+        if actor_key in learned_separate:
+            print("Actor: " +actor_key)
+
+            Rensa = make_brain(sum(learned_separate[actor_key],[]))
+            current_assertion_list = get_actor_assertions(actor_key, Rensa)
+            new_list = delete_assertion(current_assertion_list, Rensa)
+            file_name =actor_key+"_out.txt"
+            target = open(file_name, 'w')
+            for a in new_list:
+                #assertion_index_dict[str(list_count)]
+                target.write(json.dumps(a.realize(Rensa,False)))
+                target.write("\n")
+                
+            target.close()
         #Rensa = make_brain(sum(learned_separate[key],[]))
     #print "* * *\nProcess completed."
     
 def delete_assertion(old_list, Rensa):
     # this function keep reading user input to delete assertion_index_dict
-    print_asseartions(old_list, Rensa)
-    text = raw_input()
-    print(text)
+
+    text = ""
+    while text != "END" and len(old_list) > 0:
+        print_asseartions(old_list, Rensa)
+        print("\n enter numbers: ")
+        text = raw_input()
+        if text != "END":
+            try:
+                val = int(text)
+                old_list.pop(int(text))
+            except ValueError:
+                print("That's not an int!")
+        else:
+            print("return new assertions!")
+
     new_list = old_list
+    print_asseartions(new_list, Rensa)
+    
     return new_list
     
 def print_asseartions(current_list, Rensa):
