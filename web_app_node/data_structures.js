88494868f245655e4919e9cbac005cf150bd4c47
Yi-Chun Chen <ychen74@ncsu.edu>
2017-09-24 22:40:31 -0400

    clean files

--- web_app_node/data_structures.js
+++
@@ -1,9 +0,0 @@
-function story(name, path) {
-	this.name = name;
-	this.path = path;
-	this.json = path.replace(".txt", ".json");
-	this.length = -1;
-	this.character_list = [];
-	this.location_list = [];
-	this.story_point_max = -1;
-}
