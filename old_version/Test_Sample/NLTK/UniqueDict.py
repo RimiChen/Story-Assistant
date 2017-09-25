0554aeddbae4d16fc89fc38c0dab78652f6b1f7d
Yi-Chun Chen <ychen74@ncsu.edu>
2017-09-24 22:35:25 -0400

    clean files

--- old_version/Test_Sample/NLTK/UniqueDict.py
+++
@@ -1,6 +0,0 @@
-class UniqueDict(dict):
-    def __setitem__(self, key, value):
-        if key not in self:
-            dict.__setitem__(self, key, value)
-        else:
-            raise KeyError("Key already exists")
